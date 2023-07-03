a0f71b0b7681617d718c12fea93d9abe399a9cd9
u9g <git@u9g.dev>
2023-07-03 20:24:32 +0000

    don't keep iterating after finish in generic_iterator

--- src/adapter/generic_iterator.rs
+++ src/adapter/generic_iterator.rs
@@ -7,8 +7,8 @@ where
     current_page_iter: IntoIter<T>,
     item_ix_in_page: usize,
     current_page_ix: i64,
-    max_page_ix: i64,
     getter: Box<F>,
+    can_continue: bool,
 }
 
 impl<T, F> GenericIterator<T, F>
@@ -16,19 +16,22 @@ where
     F: Fn(i64) -> Vec<T>,
 {
     pub fn new(getter: Box<F>) -> Self {
+        // keep in sync with increment_page_and_get_next_page code
         let current_page = getter(1);
         Self {
             getter,
             item_ix_in_page: 0,
             current_page_ix: 1,
-            max_page_ix: 9999,
+            can_continue: !current_page.is_empty(),
             current_page_iter: current_page.into_iter(),
         }
     }
 
     fn increment_page_and_get_next_page(&mut self) {
         self.current_page_ix += 1;
-        self.current_page_iter = (self.getter)(self.current_page_ix).into_iter()
+        let new_page = (self.getter)(self.current_page_ix);
+        self.can_continue = !new_page.is_empty();
+        self.current_page_iter = new_page.into_iter()
     }
 }
 
@@ -39,12 +42,14 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next) = self.current_page_iter.next() {
-            Some(next)
-        } else if self.current_page_ix < self.max_page_ix {
-            self.increment_page_and_get_next_page();
-            self.item_ix_in_page = 0;
-            self.next()
+        if self.can_continue {
+            if let Some(next) = self.current_page_iter.next() {
+                Some(next)
+            } else {
+                self.increment_page_and_get_next_page();
+                self.item_ix_in_page = 0;
+                self.next()
+            }
         } else {
             None
         }
